489f4c276d43c952974ccebee3c449f27782a368
wuwenyu <johnnywwy@gmail.com>
2023-10-27 15:28:30 +0800

    feat：实现 add 命令

---
+++ index.js
@@ -0,0 +1,27 @@
+const program = require('commander');
+
+program
+  .option('-x, --xxx', 'what the x')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+program
+  .command('add <taskName> [添加命令]')
+  .description('add a task [添加一个任务]')
+  .action((...args) => {
+    const words = args.slice(0, -1).join(' ')
+    console.log(words);
+  });
+
+program
+  .command('clear <taskName> [删除命令]')
+  .description('clear a task [删除任务]')
+  .action(() => {
+    // const words = args.slice(0, -1).join(' ')
+    console.log('this is clear');
+  });
+
+program.parse(process.argv);
+
+console.log(program.xxx);
+console.log('这里');
